dacf7de9d11dcc093cff57d63406124ab8ac169a
TheArturthur <arturo.vidal.96@gmail.com>
2018-04-01 14:21:49 +0200

    ej6

--- ejercicios-cortos/AlmacenN.todo.java
+++ ejercicios-cortos/AlmacenN.todo.java
@@ -2,6 +2,7 @@ import es.upm.babel.cclib.Producto;
 import es.upm.babel.cclib.Almacen;
 
 // TODO: importar la clase de los semáforos.
+import es.upm.babel.cclib.Semaphore;
 
 /**
  * Implementación de la clase Almacen que permite el almacenamiento
@@ -9,54 +10,71 @@ import es.upm.babel.cclib.Almacen;
  * simultáneo del almacén por varios threads.
  */
 class AlmacenN implements Almacen {
-   private int capacidad = 0;
-   private Producto[] almacenado = null;
-   private int nDatos = 0;
-   private int aExtraer = 0;
-   private int aInsertar = 0;
+    private int capacidad = 0;
+    private Producto[] almacenado = null;
+    private int nDatos = 0;
+    private int aExtraer = 0;
+    private int aInsertar = 0;
 
-   // TODO: declaración de los semáforos necesarios
+    // TODO: declaración de los semáforos necesarios
+    private Semaphore semaphore= null;
 
-   public AlmacenN(int n) {
-      capacidad = n;
-      almacenado = new Producto[capacidad];
-      nDatos = 0;
-      aExtraer = 0;
-      aInsertar = 0;
+    public AlmacenN(int n) {
+        capacidad = n;
+        almacenado = new Producto[capacidad];
+        nDatos = 0;
+        aExtraer = 0;
+        aInsertar = 0;
 
-      // TODO: inicialización de los semáforos
-   }
+        // TODO: inicialización de los semáforos
+        semaphore= new Semaphore(1);
+    }
 
-   public void almacenar(Producto producto) {
-      // TODO: protocolo de acceso a la sección crítica y código de
-      // sincronización para poder almacenar.
+    public void almacenar(Producto producto) {
+        // TODO: protocolo de acceso a la sección crítica y código de
+        // sincronización para poder almacenar.
+        while (true) {
+            semaphore.await();
+            if (nDatos == capacidad)
+                semaphore.signal(); //If there's no space, wait for signal
+            else
+                break;//If there's space, fill
+        }
 
-      // Sección crítica
-      almacenado[aInsertar] = producto;
-      nDatos++;
-      aInsertar++;
-      aInsertar %= capacidad;
+        // Sección crítica
+        almacenado[aInsertar] = producto;
+        nDatos++;
+        aInsertar++;
+        aInsertar %= capacidad;
 
-      // TODO: protocolo de salida de la sección crítica y código de
-      // sincronización para poder extraer.
-   }
+        // TODO: protocolo de salida de la sección crítica y código de
+        // sincronización para poder extraer.
+        semaphore.signal();
+    }
 
-   public Producto extraer() {
-      Producto result;
+    public Producto extraer() {
+        Producto result;
 
-      // TODO: protocolo de acceso a la sección crítica y código de
-      // sincronización para poder extraer.
+        // TODO: protocolo de acceso a la sección crítica y código de
+        // sincronización para poder extraer.
+        while (true) {
+            sempahore.await();
+            if(nDatos==0)
+                semaphore.signal();//If no products, wait
+            else
+                break;//If there are products, consume
+        }
+        // Sección crítica
+        result = almacenado[aExtraer];
+        almacenado[aExtraer] = null;
+        nDatos--;
+        aExtraer++;
+        aExtraer %= capacidad;
 
-      // Sección crítica
-      result = almacenado[aExtraer];
-      almacenado[aExtraer] = null;
-      nDatos--;
-      aExtraer++;
-      aExtraer %= capacidad;
+        // TODO: protocolo de salida de la sección crítica y código de
+        // sincronización para poder almacenar.
+        semaphore.signal();
 
-      // TODO: protocolo de salida de la sección crítica y código de
-      // sincronización para poder almacenar.
-
-      return result;
-   }
+        return result;
+    }
 }
