23920fae766739270343e15131d3fb1c2b9dcc0b
TheArturthur <arturo.vidal.96@gmail.com>
2018-05-15 12:39:05 +0200

    15/05/2018 Created classes QuePasaCSP.java and QuePasaMonitor.java Created package cc.qp and all files moved in correctly Everything at the ready

--- QuePasa/codigo/cc/qp/QuePasa.java
+++ QuePasa/codigo/cc/qp/QuePasa.java
@@ -4,17 +4,18 @@ package cc.qp;
 
 public interface QuePasa {
 
-  public void crearGrupo(int creadorUid, String grupo)
-    throws PreconditionFailedException;
+    public void crearGrupo(int creadorUid, String grupo)
+            throws PreconditionFailedException;
 
-  public void anadirMiembro(int creadorUid, String grupo, int nuevoMiembroUid)
-    throws PreconditionFailedException;
-  
-  public void salirGrupo(int miembroUid, String grupo)
-    throws PreconditionFailedException;
 
-  public void mandarMensaje(int remitenteUid, String grupo, Object contenidos)
-    throws PreconditionFailedException;
-  
-  public Mensaje leer(int uid);
+    public void anadirMiembro(int creadorUid, String grupo, int nuevoMiembroUid)
+            throws PreconditionFailedException;
+
+    public void salirGrupo(int miembroUid, String grupo)
+            throws PreconditionFailedException;
+
+    public void mandarMensaje(int remitenteUid, String grupo, Object contenidos)
+            throws PreconditionFailedException;
+
+    public Mensaje leer(int uid);
 }
